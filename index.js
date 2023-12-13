c3784fce35eaaac5ccabb5cc2cb98958f3996c05
pyojeyn <jeynpyo@gmail.com>
2023-12-13 16:39:17 +0900

    first commit 입니다!

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    "안녕하세요! Jenkins 공부를 하기 위해 Node.js 로 서버를 만들었습니다!👏🏼"
+  );
+});
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+});
