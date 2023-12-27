311b1d5fe9a49e89850903b9ad8e3364594f240a
pyojeyn <jeynpyo@gmail.com>
2023-12-27 16:43:12 +0900

    테스트트트

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send(
-    "안녕하세요! Jenkins 공부를 하기 위해 Node.js 로 서버를 만들었습니다!👏🏼"
+    "안녕하세요! Jenkins 공부를 하기 위해 Node.js 로 서버를 만들었습니다! 그리고 12-27 테스트해봤슈👏🏼"
   );
 });
 
